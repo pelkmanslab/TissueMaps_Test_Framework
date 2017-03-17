73ed3078098b4f61f9d95462913eb51eb850fe23
Saadia Iftikhar <s.iftikhar06@gmail.com>
2017-03-17 16:07:01 +0100

    more changes ...

--- tm_test_framework.py
+++ tm_test_framework.py
@@ -36,7 +36,7 @@ project_name = 'testData'
 workflow_filename = "/home/ubuntu/testData/workflow_description.yml"
 ImageAnalysis_pipeline = os.path.join(MOCK_PATH, 'pipeline.yaml')
 project_path = os.path.join(ROOT, project_name)
-TIFF_FILES = glob.glob(os.path.join(test_data_path , '*.tif'))
+TIFF_FILES = test_data_path
 handles_path = os.path.join(project_path, 'handles')
 handles = glob.glob(os.path.join(handles_path, '*.*'))
 
@@ -91,7 +91,7 @@ class TMsTestFramework(unittest.TestCase):
         
         # create a test environment 
         
-        self.age_file(self,TIFF_FILES, 10)
+        self.age_file(self,TIFF_FILES)
         
         basepath = '/tmp/test'
         t = TestFileEnvironment(basepath, start_clear=False)
