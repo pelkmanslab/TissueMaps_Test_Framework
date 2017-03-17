d10297dc51a8931ee181e1d0990b14a5233473d0
Saadia Iftikhar <s.iftikhar06@gmail.com>
2017-03-17 14:33:40 +0100

    modified test framework python script
    modified the test framework python script …

--- tm_test_framework.py
+++ tm_test_framework.py
@@ -14,22 +14,53 @@ compared versus expected outcome.
 '''
 
 import os
+import glob
 import time
-import re
+#import re
 from scripttest import TestFileEnvironment
 import shutil
-from tmclient.api import tmclient
+#from tmclient.api import tmclient
+from tmclient.api import TmCleint
+
 import unittest
+
+host = '172.23.102.218'
+port = 8002
+username = 'test'
+password = 123456
 
 ROOT = os.path.dirname(__file__)
 MOCK_PATH = os.path.join(ROOT, 'mock')
-Project_Description_pipeline = 'expectation.yaml'
+test_data_path = "/home/ubuntu/testData"
+project_name = 'testData'
+workflow_filename = "/home/ubuntu/testData/workflow_description.yml"
 ImageAnalysis_pipeline = os.path.join(MOCK_PATH, 'pipeline.yaml')
-project_path = ''
-TIFF_FILES = 'http://testdata.tissuemaps.org/storage/' \
-           'experiment1/'
+project_path = os.path.join(ROOT, project_name)
+TIFF_FILES = list(glob.glob(test_data_path/'*.tif'))
+handles_path = os.path.join(project_path, 'handles')
+handles = list(glob.glob(handles_path/'*.*'))
+
+client = TmCleint()
 
 class TMsTestFramework(unittest.TestCase):
+    
+    def __init__(self,host,port,username,password):
+        
+        '''
+        Initialize the tm test framework with experiment description.'''
+        
+        super(TMsTestFramework,self).__init__(host,port,username,password)
+        
+        self.workflow_type = 'canonical'
+        self.microscope_type = 'cellvoyager'
+        self.plate_format = 384
+        self.plate_acquisition_mode = 'basic'
+        self.description = "test plate"
+        self.experiment_name = 'experiment_test_2D'
+        self.plate_name = 'plate1'
+        self.acq_name = 'acq1'
+        self.data_directory = 'test_data_path'
+        
     
     def age_file(self,filepath, aging=600):
         '''Make last modification and access time of the file look older.'''
@@ -38,105 +69,84 @@ class TMsTestFramework(unittest.TestCase):
         new_time = current_time - aging
         os.utime(filepath, (new_time, new_time))
         assert current_time - os.path.getmtime(filepath) >= aging
-
-#    def install_tmset(self):
-#        '''Run the installation scipt.'''
-#    
-#    def build_container(self):
-#        '''Run the scipt to create the virtual machine or docker.'''
-#    
-#    def strat_container(self):
-#        '''Start the created container.'''
-#        
-#    def stop_container(self):
-#        '''Stop the created container after the jobs are finished.'''
-#    
-#    def mount_shares(self):
-#        '''Mount the data shares.'''
-    
-    def read_experiment_description(self):
-        '''Read the contents of experiment provided by the user in  
-        expectation.yaml.'''
-        
-        lines2 = tuple(open(os.path.join(project_path,'expectation.yml'), 'r'))
-        array12 = list()
-        list22 = ['n_microscpe_files', 'n_channels', 'n_object_types' \
-                'workflow_type', 'microscope_type', 'plate_format',  \
-                'plate_acquisition_mode', 'plate_name', \
-                'description', 'acq_name', 'directory']
-        
-        # workflow_type, microscope_type, plate_format, plate_acquisition_mode
-        # project_path, plate_name, description, acq_name, directory
-        array12.append(list22)
-        i1 = 0
-        for line2 in range(1,len(lines2)):
-            a1 = re.findall(list22[i1], lines2[line2])
-            i1 += 1            
-        
-        
-#    def init_experiment_details(self):
-#        '''Create experiment, plate, acquisition etc. using an expectation.yaml 
-#        file provided by the user in the project folder'''
-#        
-#        client = tmclient()
         
     def copy_config_files(self):
         '''Copy yaml files into the project folder, eg. expectation.yml'''
         
-        shutil.copy(Project_Description_pipeline, project_path)
+        shutil.copy(workflow_filename, project_path)
         shutil.copy(ImageAnalysis_pipeline, project_path)
-        
-        
-    def test_workflow(self):
+        shutil.copy(handles, handles_path)    
+    
+    
+    def main(self):
         '''Test the different stages of tissuemaps framework '''
         
-        age_file(self,TIFF_FILES)
+        # create a test environment 
         
-        client = tmclient()
+        self.age_file(self,TIFF_FILES)
         
         basepath = '/tmp/test'
         t = TestFileEnvironment(basepath, start_clear=False)
-        project_path = os.path.join(basepath, 'tiny_canonical')
+        
+        # create an experiment in tissuemaps 
         
         result = t.run(
-                    client.create_experiment(self, workflow_type,
-                                             microscope_type, plate_format,
-                                             plate_acquisition_mode)
+                    client.create_experiment(self, self.workflow_type,
+                                             self.microscope_type, 
+                                             self.plate_format,
+                                             self.plate_acquisition_mode)
+                    .format(p=project_path),expect_stderr=True)
+        self.assertEqual(result.returncode, 0)
+        
+        # create a plate in experiment
+            
+        result = t.run(
+                    client.create_plate(self, self.plate_name, description='')
                     .format(p=project_path),
                     expect_stderr=True)
         self.assertEqual(result.returncode, 0)
-            
+        
+        # create an acquisition in plate
         
         result = t.run(
-                    client.create_plate(self, plate_name, description)
-                    .format(p=project_path),
-                    expect_stderr=True)
-        self.assertEqual(result.returncode, 0)
-        
-        result = t.run(
-            client.create_acquisition(self,plate_name,acq_name,description='')
+            client.create_acquisition(self,self.plate_name,
+                                      self.acq_name,description='')
             .format(p=project_path),
                     expect_stderr=True)
         self.assertEqual(result.returncode, 0)
         
+        # get the list of microscope files
+        
         result = t.run(
-            client.get_microscope_files(self, plate_name, acq_name)
+            client.get_microscope_files(self, self.plate_name, self.acq_name)
             .format(p=project_path),
                     expect_stderr=True)
         self.assertEqual(result.returncode, 0)
         
+        # upload the microscope files in the experiment  
+        
         result = t.run(
-            client.upload_microscope_files(self,plate_name, acq_name,directory)
+            client.upload_microscope_files(self,self.plate_name, 
+                                            self.acq_name,self.data_directory)
             .format(p=project_path),
                     expect_stderr=True)
         self.assertEqual(result.returncode, 0)
         
+#        result = t.run(
+#            self.copy_config_files(self)
+#            .format(p=project_path),
+#                    expect_stderr=True)
+#        self.assertEqual(result.returncode, 0)
+        
+        # copy workflow description file into the experiment
+        
         result = t.run(
-            copy_config_files(self)
+            client.upload_workflow_description(self,workflow_filename)
             .format(p=project_path),
                     expect_stderr=True)
         self.assertEqual(result.returncode, 0)
-            
+        
+        # submit the workflow
         
         result = t.run(
             client.submit_workflow(self, description=None)
@@ -144,10 +154,19 @@ class TMsTestFramework(unittest.TestCase):
                     expect_stderr=True)
         self.assertEqual(result.returncode, 0)
         
+        # get the status of the workflow 
+        
         result = t.run(
             client.get_workflow_status(self, depth=2)
             .format(p=project_path),
                     expect_stderr=True)
         self.assertEqual(result.returncode, 0)
 
-                
+        # download the feature values
+        
+#        result = t.run(
+#            client.download_feature_values(self, mapobject_type_name)
+#            .format(p=project_path),
+#                    expect_stderr=True)
+#        self.assertEqual(result.returncode, 0)
+        
